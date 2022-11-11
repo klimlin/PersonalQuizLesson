d23f2c80adeda30b252de69b61fb2dbb4708eedc
Alina <linalevel5@gmail.com>
2022-11-11 21:27:32 +0300

    First version

--- PersonalQuizLesson/ViewControllers/ResultViewController.swift
+++ PersonalQuizLesson/ViewControllers/ResultViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+        
+    @IBOutlet var resultLabel: UILabel!
     
     var resultAnswers: [Answer] = []
     
@@ -19,6 +21,8 @@ class ResultViewController: UIViewController {
         for answer in resultAnswers {
             print("\(answer.title) - \(answer.animal)")
         }
+        
+        showResult(resultAnimal: countResults())
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
@@ -26,6 +30,49 @@ class ResultViewController: UIViewController {
         // view.window?.rootViewController?.dismiss(animated: true)
     }
     
+    private func countResults() -> String {
+        var countDog = 0
+        var countCat = 0
+        var countRabbit = 0
+        var countTurtle = 0
+        var maxNumber = 0
+        var resultAnimal = ""
+        
+        for answer in resultAnswers {
+            if answer.animal.definition == "🐶" {
+                countDog += 1
+            } else if answer.animal.definition == "🐱" {
+                countCat += 1
+            } else if answer.animal.definition == "🐰" {
+                countRabbit += 1
+            } else if answer.animal.definition == "🐢" {
+                countTurtle += 1
+            }
+        }
+        print(countDog, countCat, countRabbit, countTurtle)
+        
+        for animalItem in [countDog, countCat, countRabbit, countTurtle] {
+            if animalItem >= maxNumber {
+                maxNumber = animalItem
+            }
+        }
+        
+        if countDog == maxNumber {
+            resultAnimal = "🐶"
+        } else if countCat == maxNumber {
+            resultAnimal = "🐱"
+        } else if countRabbit == maxNumber {
+            resultAnimal = "🐰"
+        } else if countTurtle == maxNumber {
+            resultAnimal = "🐢"
+        }
+        
+        return resultAnimal
+    }
+    
+    private func showResult(resultAnimal: String) {
+        resultLabel.text = "Вы - \(resultAnimal)!"
+    }
     
 
 }
